8bc8f3993e863dd1a5340e9f82586652e64c32e5
markk39006 <markk39006@gmail.com>
2021-04-24 15:27:24 +0700

    1

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int g = 1;
+            while (g <= c)
+            {
+                
+                for (int i = 1; i <= b; i++)
+                {
+                    for (int j = 1; j <= g*a; j++)
+                    {
+                        Console.Write(x);
+
+                    }
+                    Console.WriteLine();
+                }
+                g++;
+            }
+          
+        }
+    }
+}
